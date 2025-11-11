c0d9ffeafe46c4b3b8900e8a15e9cbafd0ea0b29
Pavel <76658162+mapanit@users.noreply.github.com>
2025-11-11 23:47:30 +0300

    Update Tools.jsx

--- frontend/src/Component/Pages/Home/Tools/Tools.jsx
+++ frontend/src/Component/Pages/Home/Tools/Tools.jsx
@@ -17,18 +17,7 @@ const Tools = ({ setBtn, btn }) => {
         WhatWeb
       </button>
       <button type="button" className="menu__active-btn">
-        WhoIS
-=======
-const Tools = () => {
-  return (
-    <div>
-      <button type="button" className="search__menu-btn">
-        Сайт
-      </button>
-      <button className="search__still" type="button" aria-label="Ещё">
-        <p>Еще</p>
-        <span />
->>>>>>> 57ef5a318f1ddaa9be9ede79b844307d66c2bafb
+        WhoIS\
       </button>
     </div>
   );
